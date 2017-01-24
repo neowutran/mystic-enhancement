df390915053f96a07269c7c53807d31e99b71a84
Neowutran <me@neowutran.ovh>
2017-01-24 23:38:52 +0100

    second release

--- index.js
+++ index.js
@@ -18,18 +18,29 @@ var server = app.listen(9999, function () {
 module.exports = function MysticEnhancement(dispatch){
 	
 	let myLocation; 
+	let lock = false;
 	dispatch.hook('cPlayerLocation', (event) => {
+		if(lock === false){
 			myLocation = event;
 			return true;
+		}
+		return false;
 	});
 
     dispatch.hook('cStartSkill', (event) => {
 		console.info(event);
-		myLocation.w = cameraAngle;
-        dispatch.toServer('cPlayerLocation', myLocation);
 		if(event.skill == 67440264 ){ //get mystic vengeance thrall skill id
-			console.info("Modify thrall: "+event.w +" -> "+cameraAngle);
+			lock = true;
+			var newLocation = JSON.parse(JSON.stringify(myLocation))
+			myLocation.w = cameraAngle;
 			event.w = cameraAngle;
+			dispatch.toServer('cPlayerLocation', myLocation);
+			dispatch.toServer('cStartSkill', event);
+			console.info("Modify thrall: "+event.w +" -> "+cameraAngle);
+			setTimeout(function(){
+				lock = false;
+			}, 800);
+			return false;
 		}
 		
         return true;
