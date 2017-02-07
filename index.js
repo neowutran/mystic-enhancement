b411a121b1f87094cddb32213329809d96731d8c
Neowutran <me@neowutran.ovh>
2017-02-07 15:01:15 +0100

    camera module

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 var express = require('express');
+var Camera = require('camera');
 var app = express();
 var fs = require("fs");
-var cameraAngle = 0;
-
-app.get('/camera', function (req, res) {
-   cameraAngle = req.query.angle;
-   res.end( "" );
-})
-
-var server = app.listen(9999, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Listening camera modification at http://%s:%s", host, port)
-});
-
 	
 module.exports = function MysticEnhancement(dispatch){
-	
+	const camera = new Camera(false);
 	let myLocation; 
 	let lock = false;
 	dispatch.hook('cPlayerLocation', (event) => {
@@ -28,15 +16,15 @@ module.exports = function MysticEnhancement(dispatch){
 	});
 
     dispatch.hook('cStartSkill', (event) => {
-		console.info(event);
 		if(event.skill == 67440264 ){ //get mystic vengeance thrall skill id
 			lock = true;
-			var newLocation = JSON.parse(JSON.stringify(myLocation))
-			myLocation.w = cameraAngle;
-			event.w = cameraAngle;
+			var newLocation = JSON.parse(JSON.stringify(myLocation));
+			console.log(camera);
+			myLocation.w = camera.angle();
+			event.w = camera.angle();
+			console.info("Modify thrall: "+event.w +" -> "+camera.angle());
 			dispatch.toServer('cPlayerLocation', myLocation);
 			dispatch.toServer('cStartSkill', event);
-			console.info("Modify thrall: "+event.w +" -> "+cameraAngle);
 			setTimeout(function(){
 				lock = false;
 			}, 800);
