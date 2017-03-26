b36ef2b6f449f5ef7cac1aa315245ce694456a4c
Mie O <miei1@outlook.com>
2017-03-26 04:45:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function MysticEnhancement(dispatch){
 	const camera = new Camera(false);
 	let myLocation; 
 	let lock = false;
-	dispatch.hook('cPlayerLocation', (event) => {
+	dispatch.hook('C_PLAYER_LOCATION', 1, (event) => {
 		if(lock === false){
 			myLocation = event;
 			return true;
@@ -15,7 +15,7 @@ module.exports = function MysticEnhancement(dispatch){
 		return false;
 	});
 
-    dispatch.hook('cStartSkill', (event) => {
+    dispatch.hook('C_START_SKILL', 1, (event) => {
 		if(event.skill == 67440264 ){ //get mystic vengeance thrall skill id
 			lock = true;
 			var newLocation = JSON.parse(JSON.stringify(myLocation));
@@ -23,8 +23,8 @@ module.exports = function MysticEnhancement(dispatch){
 			myLocation.w = camera.angle();
 			event.w = camera.angle();
 			console.info("Modify thrall: "+event.w +" -> "+camera.angle());
-			dispatch.toServer('cPlayerLocation', myLocation);
-			dispatch.toServer('cStartSkill', event);
+			dispatch.toServer('C_PLAYER_LOCATION', 1, myLocation);
+			dispatch.toServer('C_START_SKILL', 1, event);
 			setTimeout(function(){
 				lock = false;
 			}, 800);
