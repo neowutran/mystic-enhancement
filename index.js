ceb5341a1f0efada96664a55d0730dcba13f6fd5
Yukikoo <me@neowutran.ovh>
2018-07-26 17:14:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var Camera = require('camera');
 var app = express();
 var fs = require("fs");
-	
+const Vec3 = require('vec3');	
+
 module.exports = function MysticEnhancement(dispatch){
 	const camera = new Camera(false);
 	let myLocation; 
@@ -22,24 +23,24 @@ module.exports = function MysticEnhancement(dispatch){
         return parseInt(Math.hypot(x2-x1, y2-y1));
    }
    
-    dispatch.hook('C_START_SKILL', 2, (event) => {
-		if(event.skill == 67278964){
-			event.movementkey = 0;
+    dispatch.hook('C_START_SKILL', 5, (event) => {
+		console.log(event.w, event.loc.y);
+		if(event.skill == 67278964 || event.skill == 67548964){
+			event.moving = false;
+			event.continue = false;
 			event.w = camera.angle();
-			var dist = getDist(event.x1, event.y1, event.x2, event.y2);
-			event.x2 = event.x1 + dist * Math.cos(getRadian(event.w));
-			event.y2 = event.y1 + dist * Math.sin(getRadian(event.w));
-			event.z2 = event.z1 + 4;
-			dispatch.toServer('C_START_SKILL', 2, event);
+			var dist = getDist(event.loc.x, event.loc.y, event.dest.x, event.dest.y);
+			event.dest = new Vec3( event.loc.x + dist * Math.cos(getRadian(event.w))  ,  event.loc.y + dist * Math.sin(getRadian(event.w)), event.loc.z);
+			dispatch.toServer('C_START_SKILL', 5, event);
 			return false;
 		}
-		if(event.skill == 67440064){ //get mystic vengeance thrall skill id
+		if(event.skill == 67440064){ // || event.skill == 67440094 || event.skill == 67380194 || event.skill == 67449594){ //get mystic vengeance thrall skill id
 			lock = true;
 			var newLocation = JSON.parse(JSON.stringify(myLocation));
 			myLocation.w = camera.angle();
 			event.w = camera.angle();
 			dispatch.toServer('C_PLAYER_LOCATION', 1, myLocation);
-			dispatch.toServer('C_START_SKILL', 2, event);
+			dispatch.toServer('C_START_SKILL', 5, event);
 			setTimeout(function(){
 				lock = false;
 			}, 800);
